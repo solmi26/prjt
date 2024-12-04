f37fef10806b31f1a7059555bdfa85b93d73fa05
solmi <tom526@naver.com>
2024-12-04 10:44:18 +0900

    first commit

---
+++ src/test/java/com/jpa/app/PrjtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jpa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrjtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
