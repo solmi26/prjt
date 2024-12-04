56a585fd28b929a2af3701e9b83a129320229187
solmi <tom526@naver.com>
2024-12-04 15:30:24 +0900

    main update

--- src/main/java/com/jpa/app/PrjtApplication.java
+++ src/main/java/com/jpa/app/PrjtApplication.java
@@ -1,5 +1,7 @@
 package com.jpa.app;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,18 +9,22 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
 @Controller
 @SpringBootApplication
 public class PrjtApplication {
 
 	@Value("${file.uploadpath}")
 	String uploadPath;
+	private static final Logger logger = LoggerFactory.getLogger(PrjtApplication.class);
+	
 	public static void main(String[] args) {
 		SpringApplication.run(PrjtApplication.class, args);
 	}
 	
 	@GetMapping("/")
 	public String main(Model model) {
+		logger.info("main");
 		model.addAttribute("upload", uploadPath);
 		return "main";
 	}
